bd3f34b66d53d5c13ca8581710cd2e26e5fe47b5
AchintyaX <achintyashankhdhar@gmail.com>
2019-02-14 14:12:17 +0530

    base template completion

--- myapp/models.py
+++ myapp/models.py
@@ -23,7 +23,7 @@ class Order(models.Model):
     clientName = models.CharField(max_length=200)
     start_date = models.DateTimeField( default=timezone.now)
     completion_date = models.DateTimeField( blank=True, null=True)
-    Reason_delay = models.TextField(null=True, default="")
+    Reason_delay = models.TextField(null=True, blank=True)
     delay = models.DecimalField( blank=True, null=True, decimal_places=0, max_digits=2)
     order_status = models.CharField( max_length=2, choices=ORDER_STATUS, default=APPROVED)
 
