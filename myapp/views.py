10d36aff50d03f813c09d1cc59710d83d72189d7
AchintyaX <achintyashankhdhar@gmail.com>
2019-02-14 12:02:09 +0530

    created first view

--- myapp/views.py
+++ myapp/views.py
@@ -4,5 +4,5 @@ from django.utils import timezone
 
 # Create your views here.
 def order_list(request):
-    orders = Post.objects.filter(completion_date__lte=timezone.now()).order_by('completion_date')
+    orders = Order.objects.filter(start_date__lte=timezone.now()).order_by('start_date')
     return render( request, 'myapp/order_list.html', {'orders': orders})
