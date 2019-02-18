3bde8875eaf978e1ed5157af4c70b1c04324b740
AchintyaX <achintyashankhdhar@gmail.com>
2019-02-18 13:45:05 +0530

    working with user authentication

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls')), 
+    path('admin/', admin.site.urls), 
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('', include('myapp.urls')),
 ]
