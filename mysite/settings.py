da735064d3576200fadf8c630d2c1ff52c1d8066
AchintyaX <achintyashankhdhar@gmail.com>
2019-02-19 22:31:58 +0530

    creating login

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 from django.contrib.auth import views
 
 urlpatterns = [
