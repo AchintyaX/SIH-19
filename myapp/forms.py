84f480dee41b6a63c144758f73f453217591df1a
AchintyaX <achintyashankhdhar@gmail.com>
2019-02-24 21:49:37 +0530

    new form buildup begins

--- myapp/forms.py
+++ myapp/forms.py
@@ -5,3 +5,6 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('OrderID','clientName', 'start_date', 'completion_date', 'order_details', 'Reason_delay',)
+
+class UpdateOrderForm(forms.Form):
+    
